36f4b60c0837b72cd081351c9a69206cf33d3690
Raghu Betina <raghu@firstdraft.com>
2018-06-10 03:30:46 +0000

    Generate favorites resource with starter:resource

---
+++ db/migrate/20180610033046_create_favorites.rb
@@ -0,0 +1,13 @@
+class CreateFavorites < ActiveRecord::Migration
+  def change
+    create_table :favorites do |t|
+      t.integer :users_id
+      t.integer :products_id
+      t.integer :brands_id
+      t.integer :concerns_id
+
+      t.timestamps
+
+    end
+  end
+end
