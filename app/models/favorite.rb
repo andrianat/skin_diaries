b4c49e904af9b3a7bd6ebde23479063196f6844a
Raghu Betina <raghu@firstdraft.com>
2018-06-10 03:31:06 +0000

    Direct association between products and favorites

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,6 +1,10 @@
 class Favorite < ApplicationRecord
   # Direct associations
 
+  belongs_to :products,
+             :class_name => "Product",
+             :counter_cache => true
+
   belongs_to :brands,
              :class_name => "Brand",
              :counter_cache => true
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,10 @@
 class Product < ApplicationRecord
   # Direct associations
 
+  has_many   :favorites,
+             :foreign_key => "products_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
